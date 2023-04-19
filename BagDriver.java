2926e69f9c21146612ad2d1c4ad580742b8de8c9
Ankit Gajbhiye <106885420+Artemis-996@users.noreply.github.com>
2023-04-19 23:49:15 +0530

    Update BagDriver.java

--- BagDriver.java
+++ BagDriver.java
@@ -2,6 +2,8 @@ class BagDriver
 {
 	public static void main(String[] args) 
 	{
+		System.out.println("********* American Tourister ***********");
+
 		Bag b1 = new Bag();
 		System.out.println(b1.type);
 		System.out.println(b1.brand);
@@ -26,6 +28,34 @@ class BagDriver
 		System.out.println("Price : "+b1.price);
 		System.out.println("Model Number : "+b1.modelnumber);
 		System.out.println("Water Proof :"+ b1.waterproof);
+
+		System.out.println("********** Sky Bags **********");
+
+
+		Bag b2 = new Bag();
+		System.out.println(b1.type);
+		System.out.println(b1.brand);
+		System.out.println(b1.litcapacity);
+		System.out.println(b1.price);
+		System.out.println(b1.modelnumber);
+		System.out.println(b1.waterproof);
+
+		b1.type = "School Bag";
+		b1.brand = "Sky Bag";
+		b1.litcapacity = 20.0;
+		b1.price = 800;
+		b1.modelnumber = "SKY9508TS";
+		b1.waterproof = false;
+
+		System.out.println("********************");
+
+		System.out.println(b1.type);
+		System.out.println(b1.brand);
+		System.out.println(b1.litcapacity);
+		System.out.println(b1.price);
+		System.out.println(b1.modelnumber);
+		System.out.println(b1.waterproof);
+
 		
 
 
