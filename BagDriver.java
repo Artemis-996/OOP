4465f056ce80a1890e3a4623543a5a00783ed06c
Ankit Gajbhiye <106885420+Artemis-996@users.noreply.github.com>
2023-04-20 00:10:02 +0530

    Update BagDriver.java

--- BagDriver.java
+++ BagDriver.java
@@ -33,28 +33,28 @@ class BagDriver
 
 
 		Bag b2 = new Bag();
-		System.out.println(b1.type);
-		System.out.println(b1.brand);
-		System.out.println(b1.litcapacity);
-		System.out.println(b1.price);
-		System.out.println(b1.modelnumber);
-		System.out.println(b1.waterproof);
+		System.out.println(b2.type);
+		System.out.println(b2.brand);
+		System.out.println(b2.litcapacity);
+		System.out.println(b2.price);
+		System.out.println(b2.modelnumber);
+		System.out.println(b2.waterproof);
 
-		b1.type = "School Bag";
-		b1.brand = "Sky Bag";
-		b1.litcapacity = 20.0;
-		b1.price = 800;
-		b1.modelnumber = "SKY9508TS";
-		b1.waterproof = false;
+		b2.type = "School Bag";
+		b2.brand = "Sky Bag";
+		b2.litcapacity = 20.0;
+		b2.price = 800;
+		b2.modelnumber = "SKY9508TS";
+		b2.waterproof = false;
 
 		System.out.println("********************");
 
-		System.out.println(b1.type);
-		System.out.println(b1.brand);
-		System.out.println(b1.litcapacity);
-		System.out.println(b1.price);
-		System.out.println(b1.modelnumber);
-		System.out.println(b1.waterproof);
+		System.out.println("Type :"+b2.type);
+		System.out.println("Brand : "+b2.brand);
+		System.out.println("Liter Capacity : "+b2.litcapacity);
+		System.out.println("Price : "+b2.price);
+		System.out.println("Model Number : "+b2.modelnumber);
+		System.out.println("Water Proof :"+b2.waterproof);
 
 		
 
