100a1dbc78ccb92bf73cc3cb681952478eca1f0a
Ankeet <106885420+Artemis-996@users.noreply.github.com>
2023-05-14 00:14:03 +0530

    Add files via upload

---
+++ Inheritance/Multi-Level/University - College - Department/departmentDriver.java
@@ -0,0 +1,14 @@
+package singlelevel;
+
+public class departmentDriver {
+
+	public static void main(String[] args) {
+		
+		department d1 = new department("Pune", "PHCSA1111", "Pune", "Ronaldo", "ABC College", "Pune", "Messi", 58745, "A++", "Engineering", "CS", "Neymar", 400, 25);
+		
+		d1.displayDepartment();
+	
+
+	}
+
+}
